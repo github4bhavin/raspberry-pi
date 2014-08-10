11a542308346c11f22afeea483935c0d16aff1a0
bhavin patel <mail4bhavin@gmail.com>
2014-08-10 02:41:27 -0400

    addition

--- weather.py
+++ weather.py
@@ -1,4 +1,3 @@
-#__author__ = 'bhavinpatel'
 import RPi.GPIO as g
 import time
 import random
@@ -7,7 +6,7 @@ from pprint import pprint
 class Diode:
 
 	def __init__(self , red=12,blue=18,green=16):
-
+	
 		self.red   = red
 		self.blue  = blue
 		self.green = green
@@ -19,70 +18,74 @@ class Diode:
 			self.g.setup( self.green , g.OUT)
 		except:
 			pass
-
-
+		
+	
 	def redLed(self, status ):
 		self.g.output( self.red , status)
 		time.sleep(status)
-
+		
 	def cleanUp(self):
 		self.g.cleanup()
-
+		
 	def __del__(self):
 		self.g.cleanup()
-
+		
 class Pallet(Diode):
 
 	initialFrequency = 100
 	initialDutyCycle = 0
-
+	
 	def __init( self , red , blue , green ):
 		self = Diode.__init__(self, red, blue , green)
 		self.RED   = self.g.PWM( self.red   , self.initialFrequency)
 		self.BLUE  = self.g.PWM( self.blue  , self.initialFrequency)
 		self.GREEN = self.g.PWM( self.green , self.initialFrequency)
-
+		
 		self.RED.start(   self.initialDutyCycle )
 		self.BLUE.start(  self.initialDutyCycle )
 		self.GREEN.start( self.initialDutyCycle )
-
+	
 	def set_pwm_for_red(self):
 		self.RED = self.g.PWM( self.red , self.initialFrequency)
 		self.RED.start( self.initialDutyCycle )
-
+		
 	def set_pwm_for_blue(self):
 		self.BLUE = self.g.PWM( self.blue , self.initialFrequency)
 		self.BLUE.start( self.initialDutyCycle )
-
+		
 	def set_pwm_for_green(self):
 		self.GREEN = self.g.PWM( self.green , self.initialFrequency)
 		self.GREEN.start( self.initialDutyCycle )
-
+						
 	def mapping(self, val):
 		retval = round((100*val)/255,1)
-		pprint( retval )
+		pprint( retval ) 
 		return retval
-
-	def color (self , r, b, g ):
+		
+	def color (self , r, b, g ):	
 		if( not hasattr(self,'RED') ) :
 			self.set_pwm_for_red()
-
+		
 		if( not hasattr(self,'BLUE') ) :
 			self.set_pwm_for_blue()
-
+		
 		if( not hasattr(self,'GREEN') ) :
 			self.set_pwm_for_green()
-
-		self.RED.ChangeDutyCycle(   self.mapping( r ) )
-		self.BLUE.ChangeDutyCycle(  self.mapping( b ) )
-		self.GREEN.ChangeDutyCycle( self.mapping( g ) )
-
+				
+		self.RED.ChangeDutyCycle(   self.mapping( r ) ) 
+		self.BLUE.ChangeDutyCycle(  self.mapping( b ) ) 
+		self.GREEN.ChangeDutyCycle( self.mapping( g ) ) 		
+		
 	def wait(self ,waitPeriod=1):
 		time.sleep( waitPeriod)
-
+		
 if __name__ == "__main__":
 	p = Pallet()
-
+	
 	p.redLed(True)
-	p.color(255,0,255)
-	p.wait(10)
+	for r in range(0,255,10):
+		for b in range(0, 255,10):
+			for g in range(0,255,10):
+				print "\n r(%d), b(%d), g(%d)" %( r, b ,g )
+				p.color(r,b,g)
+				#p.wait(0.1)
